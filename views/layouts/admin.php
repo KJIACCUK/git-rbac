58b0013387df262930773be519c291367c8fd21c
KJIACCUK <mig-alex14@mail.ru>
2016-05-23 12:50:45 +0300

    rbac старт!!!

---
+++ views/layouts/admin.php
@@ -0,0 +1,99 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+use yii\helpers\Url;
+use mdm\admin\components\Helper;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+$menuItems = [
+            ['label' => 'Главная', 'url' => ['/admin/default/index']],
+            ['label' => 'Посты', 'url' => ['/admin/post/index']],
+            ['label' => 'Пользователи', 'url' => ['/rbac/default/index']],
+            Yii::$app->user->isGuest ? (
+                ['label' => 'Войти', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
+                . Html::endForm()
+                . '</li>'
+            )
+        ];
+
+    NavBar::begin([
+        'brandLabel' => 'Настроен rbac',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+
+        if(!Yii::$app->user->isGuest):
+        ?>
+    <div class="navbar-form navbar-right">
+                <button class="btn btn-sm btn-default"
+                        data-container="body"
+                        data-toggle="popover"
+                        data-trigger="focus"
+                        data-placement="bottom"
+                        data-title="<?= Yii::$app->user->identity['username'] ?>"
+                        data-content="
+                            <a href='<?= Url::to(['/site/profile']) ?>' data-method='post'>Мой профиль</a><br>
+                            <a href='<?= Url::to(['/site/profile']) ?>' data-method='post'>Настройки</a><br>
+                            <a href='<?= Url::to(['/site/logout']) ?>' data-method='post'>Выход</a>
+                        ">
+                    <span class="glyphicon glyphicon-user"></span>
+                </button>
+            </div>
+    <?php
+    endif;
+
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => Helper::filter($menuItems),
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
